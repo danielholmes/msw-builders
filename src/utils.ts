5416bdd5180cb20fa1fc3298867faa336c6de3d7
Daniel Holmes <daniel@danielholmes.org>
2024-05-08 16:09:05 +1000

    Fix json parsing for other content types

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,17 @@ async function extractBodyContent(
   requestOrResponse: Pick<Response, "headers" | "json" | "formData">,
 ): Promise<Record<string, unknown>> {
   const contentType = requestOrResponse.headers.get("Content-Type");
-  if (contentType === "application/json") {
+
+  // Json
+  // Need to take into account custom json, e.g. application/x-amz-json-1.1
+  if (
+    contentType === "application/json" ||
+    (contentType?.startsWith("application/") &&
+      contentType.split("application/")[1].includes("json"))
+  ) {
     return requestOrResponse.json();
   }
+
   if (contentType?.startsWith("multipart/form-data")) {
     const data = await requestOrResponse.formData();
     return Object.fromEntries(data.entries());
